b5e5d5893913db8e2040cea8a13167bd586af5a4
jbenererben <81986650+jbenererben@users.noreply.github.com>
2025-06-22 17:58:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+const actions = require('./actions')
 
+module.exports = {
+  name: 'p20-layerselect',
+  config: {},
+
+  init() {
+    this.log('info', 'P20 Layer Select modülü yüklendi.')
+    this.selectedScreenId = 1
+    actions(this)
+  },
+}
